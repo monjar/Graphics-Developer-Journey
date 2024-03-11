399d84452106912419f2245b8558bd7bb626a442
Amirali <amirali.mnj@gmail.com>
2024-03-11 16:24:14 +0000

    Add Cat model

--- Step 1 (How Things Work)/MiniRenderer/main.cpp
+++ Step 1 (How Things Work)/MiniRenderer/main.cpp
@@ -7,84 +7,54 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 Model *model = NULL;
-const int width = 800;
-const int height = 800;
+const int width = 1080;
+const int height = 1080;
+
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
+
 	bool steep = false;
+	//Check if the dy is Greater than dx then we need to swap it to draw more pixels in line
 	if (std::abs(x0 - x1) < std::abs(y0 - y1))
 	{
 		std::swap(x0, y0);
 		std::swap(x1, y1);
 		steep = true;
 	}
+	//We want to draw from left to right. so... yeah...
 	if (x0 > x1)
 	{
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
 
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derror2 = std::abs(dy) * 2;
+	int error2 = 0;
+	int y = y0;
+	//Go Pixel by pixel on x axis (or y but its flipped)
 	for (int x = x0; x <= x1; x++)
 	{
-		float t = (x - x0) / (float)(x1 - x0);
-		int y = y0 * (1. - t) + y1 * t;
+
+		//If its fliped set the flipped pixel
 		if (steep)
 		{
 			image.set(y, x, color);
 		}
 		else
 		{
-			image.set(x, y, color);	
-		}	
-	}	
+			image.set(x, y, color);
+		}
+		error2 += derror2;
+		if (error2 > dx)
+		{
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx * 2;
+		}
+	}
 }
-
-// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
-// {
-
-	
-// 	int dx = x1 - x0;
-// 	int dy = y1 - y0;
-
-// 	bool steep = false;
-// 	//Check if the dy is Greater than dx then we need to swap it to draw more pixels in line
-// 	if (std::abs(x0 - x1) < std::abs(y0 - y1))
-// 	{
-// 		std::swap(x0, y0);
-// 		std::swap(x1, y1);
-// 		steep = true;
-// 	}
-// 	//We want to draw from left to right. so... yeah...
-// 	if (x0 > x1)
-// 	{
-// 		std::swap(x0, x1);
-// 		std::swap(y0, y1);
-// 	}
-// 	int derror2 = std::abs(dy) * 2;
-// 	int error2 = 0;
-// 	int y = y0;
-// 	//Go Pixel by pixel on x axis (or y but its flipped)
-// 	for (int x = x0; x <= x1; x++)
-// 	{
-
-// 		//If its fliped set the flipped pixel
-// 		if (steep)
-// 		{
-// 			image.set(y, x, color);
-// 		}
-// 		else
-// 		{
-// 			image.set(x, y, color);
-// 		}
-// 		error2 += derror2;
-// 		if (error2 > dx)
-// 		{
-// 			y += (y1 > y0 ? 1 : -1);
-// 			error2 -= dx * 2;
-// 		}
-// 	}
-// }
 
 int main(int argc, char **argv)
 {
@@ -94,7 +64,7 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		model = new Model("Models/african_head.obj");
+		model = new Model("Models/cat.obj");
 	}
 
 	TGAImage image(width, height, TGAImage::RGB);
