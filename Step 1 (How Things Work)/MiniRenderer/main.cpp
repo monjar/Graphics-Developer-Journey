fe98e68f6db79d0c638f58c4420e9e644d2798db
Amirali <amirali.mnj@gmail.com>
2024-03-11 15:42:56 +0000

    Add model line-renderer

--- Step 1 (How Things Work)/MiniRenderer/main.cpp
+++ Step 1 (How Things Work)/MiniRenderer/main.cpp
@@ -1,60 +1,119 @@
+#include <vector>
+#include <cmath>
 #include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
-
-	
-	int dx = x1 - x0;
-	int dy = y1 - y0;
-
 	bool steep = false;
-	//Check if the dy is Greater than dx then we need to swap it to draw more pixels in line
 	if (std::abs(x0 - x1) < std::abs(y0 - y1))
 	{
 		std::swap(x0, y0);
 		std::swap(x1, y1);
 		steep = true;
 	}
-	//We want to draw from left to right. so... yeah...
 	if (x0 > x1)
 	{
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
-	int derror2 = std::abs(dy) * 2;
-	int error2 = 0;
-	int y = y0;
-	//Go Pixel by pixel on x axis (or y but its flipped)
+
 	for (int x = x0; x <= x1; x++)
 	{
-
-		//If its fliped set the flipped pixel
+		float t = (x - x0) / (float)(x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
 		if (steep)
 		{
 			image.set(y, x, color);
 		}
 		else
 		{
-			image.set(x, y, color);
-		}
-		error2 += derror2;
-		if (error2 > dx)
-		{
-			y += (y1 > y0 ? 1 : -1);
-			error2 -= dx * 2;
-		}
-	}
+			image.set(x, y, color);	
+		}	
+	}	
 }
+
+// void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+// {
+
+	
+// 	int dx = x1 - x0;
+// 	int dy = y1 - y0;
+
+// 	bool steep = false;
+// 	//Check if the dy is Greater than dx then we need to swap it to draw more pixels in line
+// 	if (std::abs(x0 - x1) < std::abs(y0 - y1))
+// 	{
+// 		std::swap(x0, y0);
+// 		std::swap(x1, y1);
+// 		steep = true;
+// 	}
+// 	//We want to draw from left to right. so... yeah...
+// 	if (x0 > x1)
+// 	{
+// 		std::swap(x0, x1);
+// 		std::swap(y0, y1);
+// 	}
+// 	int derror2 = std::abs(dy) * 2;
+// 	int error2 = 0;
+// 	int y = y0;
+// 	//Go Pixel by pixel on x axis (or y but its flipped)
+// 	for (int x = x0; x <= x1; x++)
+// 	{
+
+// 		//If its fliped set the flipped pixel
+// 		if (steep)
+// 		{
+// 			image.set(y, x, color);
+// 		}
+// 		else
+// 		{
+// 			image.set(x, y, color);
+// 		}
+// 		error2 += derror2;
+// 		if (error2 > dx)
+// 		{
+// 			y += (y1 > y0 ? 1 : -1);
+// 			error2 -= dx * 2;
+// 		}
+// 	}
+// }
 
 int main(int argc, char **argv)
 {
-	TGAImage image(100, 100, TGAImage::RGB);
-	line(33, 20, 80, 40, image, white);
-	line(20, 13, 40, 80, image, red);
+	if (argc == 2)
+	{
+		model = new Model(argv[1]);
+	}
+	else
+	{
+		model = new Model("Models/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++)
+		{
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.) * width / 2.;
+			int y0 = (v0.y + 1.) * height / 2.;
+			int x1 = (v1.x + 1.) * width / 2.;
+			int y1 = (v1.y + 1.) * height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
+	delete model;
 	return 0;
 }
