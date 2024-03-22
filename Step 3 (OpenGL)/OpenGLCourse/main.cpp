5f9368eb9563655fe79aaae057ae6a40b34f27b4
Amirali <amirali.mnj@gmail.com>
2024-03-22 18:50:32 +0000

    Extract main texture codes to new files

--- Step 3 (OpenGL)/OpenGLCourse/main.cpp
+++ Step 3 (OpenGL)/OpenGLCourse/main.cpp
@@ -1,7 +1,14 @@
+//------- Ignore this ----------
+#include<filesystem>
+namespace fs = std::filesystem;
+//------------------------------
+
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
+
+#include"Texture.h"
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
@@ -9,119 +16,133 @@
 
 
 
-
 // Vertices coordinates
 GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+	-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+	 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+	 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f  // Lower right corner
+};
+
+// Indices for vertices order
+GLuint indices[] =
 {
-	-0.5f, -0.5f , 0.0f,	1.0f, 0.0f, 0.0f, 1.0f,		0.0f, 0.0f,
-	-0.5f, 0.5f , 0.0f,		0.0f, 1.0f, 0.0f,1.0f, 		0.0f, 1.0f,
-	0.5f, 0.5f , 0.0f,		0.0f, 0.0f, 1.0f,1.0f,		1.0f, 1.0f,
-	0.5f, -0.5f , 0.0f,		1.0f, 0.0f, 0.0f,1.0f,		1.0f, 0.0f,
+	0, 2, 1, // Upper triangle
+	0, 3, 2 // Lower triangle
 };
 
 
-GLuint indices[] = {
-	0,2,1,
-	0,3,2
-};
 
-GLFWwindow* initWindow() {
+int main()
+{
+	// Initialize GLFW
 	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
-
-	return glfwCreateWindow(800, 800, "OpenGL Learning", NULL, NULL);
-}
-
-
-
-int main() {
-
-	
-	GLFWwindow* window = initWindow();
-	if (window == NULL) {
-		std::cout << "Failed to create window =(";
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-
-
-
-	//Initialize Glad
+	//Load GLAD so it configures OpenGL
 	gladLoadGL();
-
-	//Setting the viewport size
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
 
+
+
+	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
 
-	VAO vao1;
-	vao1.Bind();
-	VBO vbo1(vertices, sizeof(vertices));
-	EBO ebo1(indices, sizeof(indices));
-
-	vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 9 * sizeof(float), (void*)0);
-	vao1.LinkAttrib(vbo1, 1, 4, GL_FLOAT, 9 * sizeof(float), (void*)(3 * sizeof(float)));
-	vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 9 * sizeof(float), (void*)(7 * sizeof(float)));
-	vao1.Unbind();
-	vbo1.Unbind();
-	ebo1.Unbind();
-
-	GLuint uniformId = glGetUniformLocation(shaderProgram.ID, "scale");
 
 
-	int imgWidth, imgHeight, numColorChannels;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* bytes = stbi_load("birdy.png", &imgWidth, &imgHeight, &numColorChannels, 0);
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.Bind();
 
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	//S ==> X / T ==> Y / I ==> Z
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	// Unbind all to prevent accidentally modifying them
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	// Gets ID of uniform called "scale"
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, some4d vector as color);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth, imgHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(bytes);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	
+	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+	std::string texPath = "/Resources/Textures/";
 
-	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
-	shaderProgram.Activate();
-	glUniform1i(tex0Uni, 0);
-	while (!glfwWindowShouldClose(window)) {
+	// Texture
+	Texture popCat((parentDir + texPath + "birdy.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
 
-		glClearColor(0.07f, 0.07f, 0.2f, 1.0);
+	// Original code from the tutorial
+	/*Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);*/
+
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
-
+		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
-		glUniform1f(uniformId, 0.5);
-		glBindTexture(GL_TEXTURE_2D, texture);
-
-
-		vao1.Bind();
+		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+		glUniform1f(uniID, 0.5f);
+		// Binds texture so that is appears in rendering
+		popCat.Bind();
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
-
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
-	vao1.Delete();
-	vbo1.Delete();
-	ebo1.Delete();
+
+
+	// Delete all the objects we've created
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	popCat.Delete();
 	shaderProgram.Delete();
+	// Delete window before ending the program
 	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
